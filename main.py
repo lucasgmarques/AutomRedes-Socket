24b16795a0fa5ca398e809ab1e9262f12c4dbd1d
Lucas Marques <lucasg3.gdh@gmail.com>
2023-10-30 11:47:04 -0300

    Writing output to a log file

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import socket
 
-def verificar_porta(host, porta):
+def verificar_porta(host, porta, portas_abertas, portas_fechadas):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
         s.settimeout(3)
-
         s.connect((host, porta))
-
-        print(f"A porta {porta} está aberta")
-
         s.close()
+        portas_abertas.append(porta)
+        print(f"A porta {porta} está aberta")
     except (socket.timeout, ConnectionRefusedError):
+        portas_fechadas.append(porta)
         print(f"A porta {porta} está fechada")
 
 def principal():
     host = socket.gethostbyname(socket.gethostname())
+    portas_abertas = []
+    portas_fechadas = []
+
     for porta in range(1, 1025):
-        verificar_porta(host, porta)
+        verificar_porta(host, porta, portas_abertas, portas_fechadas)
+
+    with open("resultado.log", "w", encoding='utf-8') as output:
+        print("Portas abertas:", end=" ", file=output)
+        for porta in portas_abertas:
+            print(porta, end=", ", file=output)
+        print("\nPortas fechadas:", end=" ", file=output)
+        for porta in portas_fechadas:
+            print(porta, end=", ", file=output)
+        print("", file=output)
+        print(f"Número de portas abertas: {len(portas_abertas)}", file=output)
+        print(f"Número de portas fechadas: {len(portas_fechadas)}", file=output)
 
 if __name__ == "__main__":
     principal()
